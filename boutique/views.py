516ec33e5201bb7404ac15e58cf8502dd2465bd5
Ismael Loko <lokoismael30@gmail.com>
2024-11-02 09:28:52 -0400

    Adding a context processor to display all the categories

--- boutique/views.py
+++ boutique/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 def home(request):
-    categories = Category.objects.all()
-    
     #Recuperer 8 produits aleatoires
     random_products = Product.objects.order_by('?')[:8]
 
     recent_products = Product.objects.order_by('-created_at')[:8]
 
     context = {
-        'categories': categories,
         'random_products': random_products, 
         'recent_products':recent_products
     }
